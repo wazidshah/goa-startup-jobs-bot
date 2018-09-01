8033ee241dfcd749771eb04ca59d10690e4fb7fe
Nitesh Sawant <ns23@github.com>
2018-09-01 13:15:45 +0530

    Formatted data as json

--- parse.js
+++ parse.js
@@ -8,14 +8,26 @@ const $ = cheerio.load(html, {
     normalizeWhitespace: true
 })
 
+let op =[];
 $('li[id^="job_listing"]').each(function (i, elm) {
-    console.log(i);
-    
-    if (i < 4) {
-        // console.log($(this)); // for testing do text()
-        let $time = $(this).find('ul.meta>li.date');
-        // fs.writeFile('op.txt',$time.text().trim());
-        // console.log($time.text().trim());
-         
+    op.push({
+      id:$(this).attr('id').replace('job_listing-',''),
+      title:$(this).attr('data-title'),
+      type:$(this).find('ul.meta>li.job-type').text().trim(),
+      href:$(this).attr('data-href'),
+      date:$(this).find('ul.meta>li.date').text().trim()  
+    })
+});
+
+// console.log(op);
+
+let jsonContent = JSON.stringify(op,null,4);
+ 
+fs.writeFile("output.json", jsonContent, 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
     }
+ 
+    console.log("JSON file has been saved.");
 });
